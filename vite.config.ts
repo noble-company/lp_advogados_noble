9e7c4bb196ac3bf8f052a62a7e3e4a9a464f2515
Noble Company Digital <noblecompany.digital@gmail.com>
2026-01-28 13:46:52 -0300

    bla bla

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/lp-advogados/',
+  base: './',
   server: {
     host: "::",
     port: 8080,
