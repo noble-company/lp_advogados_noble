5ccd088ac0cb6e937a439bea8263445ff21e2ca0
Noble Company Digital <noblecompany.digital@gmail.com>
2026-01-28 12:40:59 -0300

    Arrumando os actions

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: process.env.GITHUB_ACTIONS ? '/lp-advogados/' : '/',
+  base: '/lp-advogados/',
   server: {
     host: "::",
     port: 8080,
