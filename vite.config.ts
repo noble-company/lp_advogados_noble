52737024d851c684eeee682c497a680d21ade97c
Noble Company Digital <noblecompany.digital@gmail.com>
2026-01-29 13:46:43 -0300

    Mudando url base

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/legal-ai-leap/',
+  base: '/lp_advogados_noble/',
   server: {
     host: "::",
     port: 8080,
